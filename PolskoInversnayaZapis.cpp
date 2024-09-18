794f433a4b2479b53d9be7550f1bd6bc8eb4e760
suproq <solod.kirill@yandex.ru>
2024-09-19 02:37:04 +0500

    Добавил комментарии + фикс, если строка вводится с пробелами и их удаление

--- PolskoInversnayaZapis.cpp
+++ PolskoInversnayaZapis.cpp
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <string> 
 
+// определяет приоритет знака у символа
 int getPriority(char symbol) {
     if (symbol == '(') {
         return 0;
@@ -20,6 +21,7 @@ int getPriority(char symbol) {
     return -1; // не знак
 }
 
+// выводит строку таблицы, вызывается после каждого шага
 void outTypeString(int step, vector<char> input, vector<char> stack, string  output) {
     string stackStr(stack.begin(), stack.end());
     vector<char> newInput;
@@ -38,48 +40,42 @@ int main()
     system("cls");
     cout << "Входная строка: ";
     string inputStr;
-    cin >> inputStr;
+    getline(cin, inputStr);
     cout << "\n";
-    vector<char> input(inputStr.begin(), inputStr.end());
+    inputStr.erase(std::remove(inputStr.begin(), inputStr.end(), ' '), inputStr.end());
+    vector<char> input(inputStr.begin(), inputStr.end()); // переведем входную строку в вектор чаров, чтобы с ней было проще работать
 
     if (count(input.begin(), input.end(), '(') != count(input.begin(), input.end(), ')')) {
         cout << "Должно быть равное количество элементов ( и )!\n";
         return 0;
     }
 
-
     string output = "";
     vector<char> stack;
 
+    //вывод шапки таблицы
     string head[]{ "Шаг", "Входная строка", "Стек", "Выходная строка" };
     cout << head[0] << setw(5 - head[0].length()) << " | " << head[1] << setw(40 - head[1].length()) << " | " << head[2] << setw(40 - head[2].length()) << " | " << head[3] << '\n';
 
-    //a+b*d/c=0
-    // a/b*d+c
-    // a+b*(d-e)=0
-    // a+(b*(d-e))
-
-
-
     outTypeString(-1, input, stack, output);
-
-
 
     for (int i = 0; i < input.size(); i++) {
         const char inputSymbol = input[i];
         const int priority = getPriority(inputSymbol);
-        if (priority == -1) {
+        // все переменные и числа сразу пеносим в выходную строку
+        if (priority == -1) { 
             output = output + inputSymbol;
             outTypeString(i, input, stack, output);
             continue;
         }
-
+        // "(" добавляем в конец стека всегда 
         if (priority == 0) {
             stack.push_back(inputSymbol);
             outTypeString(i, input, stack, output);
             continue;
         }
 
+        // обработка ")"
         if (inputSymbol == ')') {
             if (count(input.begin(), input.end(), '(') == 0) {
                 cout << ") не может стоять перед (!\n";
@@ -94,6 +90,7 @@ int main()
             continue;
         }
 
+        // обработка всех остальных случаев с обычными знаками
         while (stack.size() > 0 && priority <= getPriority(stack[stack.size() - 1]) && stack[stack.size() - 1] != '(') {
             output = output + stack[stack.size() - 1];
             stack.pop_back();
@@ -101,6 +98,8 @@ int main()
         stack.push_back(inputSymbol);
         outTypeString(i, input, stack, output);
     }
+
+    // после обработки входной строки, переносим в выходную стек
     int i = input.size();
     while (stack.size() > 0){
         output = output + stack[stack.size() - 1];
